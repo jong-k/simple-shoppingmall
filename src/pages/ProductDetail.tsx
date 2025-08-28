c6998b81afb63a901be53d2e3b09b0cfc529469a
jong-k <fully.charged07@gmail.com>
2025-08-28 20:44:10 +0900

    feat: 페이지에 필요한 데이터 페칭 테스트

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,8 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import NotFound from "./NotFound";
 
+const URL = "https://dummyjson.com/products";
+
 export default function ProductDetail() {
   const { id } = useParams<{ id: string }>();
+  const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    const url = `${URL}/${id || 1}`;
+    const fetchProduct = async () => {
+      console.log("Fetching!");
+      try {
+        const res = await axios.get(url);
+        setData(res.data);
+      } catch (err) {
+        const error = err as Error;
+        setData(error.message);
+      }
+    };
+
+    fetchProduct();
+  }, [id]);
 
   if (id == undefined || !/^\d+$/.test(id)) {
     return <NotFound />;
@@ -10,5 +31,20 @@ export default function ProductDetail() {
 
   const productId = parseInt(id);
 
-  return <div>Hello from products detail page! #{productId}</div>;
+  return (
+    <div>
+      <h2>Hello from products detail page! #{productId}</h2>
+      {data && (
+        <div className="flex gap-4">
+          <div>{data.id}</div>
+          <div>
+            <img src={data.thumbnail} />
+          </div>
+          <div>{data.title}</div>
+          <div>{data.price}</div>
+          <div>{data.tags}</div>
+        </div>
+      )}
+    </div>
+  );
 }
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,3 +1,46 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const URL = "https://dummyjson.com/products";
+
 export default function Products() {
-  return <div>Hello from Products Page</div>;
+  const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      console.log("Fetching!");
+      try {
+        const res = await axios.get(URL, {
+          params: {
+            limit: 20,
+          },
+        });
+        setData(res.data);
+      } catch (err) {
+        const error = err as Error;
+        setData(error.message);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <h2>Hello from Products Page</h2>
+      <div>
+        {data &&
+          data.products.map(p => (
+            <div key={p.id} className="flex gap-4">
+              <div>{p.id}</div>
+              <div>
+                <img src={p.thumbnail} />
+              </div>
+              <div>{p.title}</div>
+              <div>{p.price}</div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 }
