64614e6b76eb712246dd7a058fd6638fae646d94
jong-k <fully.charged07@gmail.com>
2025-08-29 15:12:59 +0900

    feat: header 컴포넌트 스타일 변경

--- src/app/layouts/Header.tsx
+++ src/app/layouts/Header.tsx
@@ -3,7 +3,7 @@ import { Container } from "../../shared/ui";
 
 export default function Header() {
   return (
-    <header className="w-full bg-white">
+    <header className="w-full bg-white py-4 text-xl font-semibold shadow-sm">
       <Container>
         <Link to="/">Simple Shoppingmall</Link>
       </Container>
