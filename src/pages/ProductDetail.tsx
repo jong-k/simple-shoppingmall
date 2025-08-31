ebf8a33ed098d07ece04f32f50ed8eb0cb1a38df
jong-k <fully.charged07@gmail.com>
2025-08-31 22:03:33 +0900

    chore: page의 불필요 classname props 제거

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,14 +1,9 @@
 import { useParams } from "react-router";
-import { twMerge } from "tailwind-merge";
 import NotFound from "./NotFound";
 import { useProductByIdQuery } from "../entities/product/model";
 import { ProductDetails } from "../entities/product/ui";
 
-interface ProductDetailProps {
-  className?: string;
-}
-
-export default function ProductDetail({ className }: ProductDetailProps) {
+export default function ProductDetail() {
   const { id } = useParams();
   const productId = Number(id);
   const { data, isLoading, isError } = useProductByIdQuery(productId);
@@ -21,7 +16,7 @@ export default function ProductDetail({ className }: ProductDetailProps) {
   if (isError) return <div>Failed to load product.</div>;
 
   return (
-    <section className={twMerge("py-4", className)}>
+    <section className="py-4">
       {data && <ProductDetails price={data.price} title={data.title} thumbnail={data.thumbnail} tags={data.tags} />}
     </section>
   );
