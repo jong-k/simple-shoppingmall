d020841a022cbe13072d0b0c0b4be2ef445f39b5
jong-k <fully.charged07@gmail.com>
2025-08-31 16:39:45 +0900

    feat: 카드 클릭시 상세 페이지로 링크

--- src/entities/product/ui/ProductCard.tsx
+++ src/entities/product/ui/ProductCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import { ProductThumbnail } from "./ProductThumbnail";
 import { Card } from "../../../shared/ui";
 import type { Product } from "../model";
@@ -8,13 +9,15 @@ interface ProductCardProps {
 
 export function ProductCard({ product }: ProductCardProps) {
   return (
-    <Card className="flex min-w-[150px] items-center gap-4">
-      <ProductThumbnail src={product.thumbnail} alt={product.title} className="w-24 md:w-28" />
+    <Link to={`/products/${product.id}`}>
+      <Card className="flex min-w-[150px] items-center gap-4 transition-shadow hover:shadow-md">
+        <ProductThumbnail src={product.thumbnail} alt={product.title} className="w-24 md:w-28" />
 
-      <div className="min-w-0 flex-1">
-        <h3 className="line-clamp-3 text-lg font-semibold tracking-tight md:text-xl">{product.title}</h3>
-        <div className="mt-1 line-clamp-1 text-sm text-slate-700 md:text-base">$ {product.price}</div>
-      </div>
-    </Card>
+        <div className="min-w-0 flex-1">
+          <h3 className="line-clamp-3 text-lg font-semibold tracking-tight md:text-xl">{product.title}</h3>
+          <div className="mt-1 line-clamp-1 text-sm text-slate-700 md:text-base">$ {product.price}</div>
+        </div>
+      </Card>
+    </Link>
   );
 }
