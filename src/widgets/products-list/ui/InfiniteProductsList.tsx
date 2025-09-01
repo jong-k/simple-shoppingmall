4e5d998a5958f710d8582bd388ab2a10de0e4f86
jong-k <fully.charged07@gmail.com>
2025-09-01 13:43:29 +0900

    feat: inview 작동 테스트

--- src/widgets/products-list/ui/InfiniteProductsList.tsx
+++ src/widgets/products-list/ui/InfiniteProductsList.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 import { twMerge } from "tailwind-merge";
 import { ProductsList } from "./ProductsList";
 import { useProductsQuery } from "../../../entities/product/model";
@@ -9,9 +11,19 @@ interface InfiniteProductsListProps {
 
 export function InfiniteProductsList({ limit, className }: InfiniteProductsListProps) {
   const { data, isLoading, isError } = useProductsQuery({ limit });
+  const { ref, inView } = useInView({ threshold: 0, rootMargin: "200px 0px" });
+
+  useEffect(() => {
+    if (inView) console.log(inView);
+  }, [inView]);
 
   if (isLoading) return <div>Loading products...</div>;
   if (isError) return <div>Failed to load products.</div>;
 
-  return <section className={twMerge("py-4", className)}>{data && <ProductsList products={data?.products} />}</section>;
+  return (
+    <section className={twMerge("py-4", className)}>
+      {data && <ProductsList products={data?.products} />}
+      <div ref={ref} className=""></div>
+    </section>
+  );
 }
