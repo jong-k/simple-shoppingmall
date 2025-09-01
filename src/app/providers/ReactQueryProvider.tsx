0d09e3fca668ce8d967a02bc14c22efe438d50da
jong-k <fully.charged07@gmail.com>
2025-09-01 21:08:53 +0900

    chore: react query gctime 기본값 제거

--- src/app/providers/ReactQueryProvider.tsx
+++ src/app/providers/ReactQueryProvider.tsx
@@ -6,7 +6,6 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 30_000,
-      gcTime: 5 * 60_000,
       retry: 1,
       refetchOnWindowFocus: false,
     },
