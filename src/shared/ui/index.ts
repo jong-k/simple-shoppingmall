3dd58d4eee082bbe3d19c34a906406e7e5f0a79f
jong-k <fully.charged07@gmail.com>
2025-09-01 14:24:40 +0900

    feat: spinner 컴포넌트 생성 및 적용

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,3 +1,4 @@
 export * from "./Container";
 export * from "./Card";
 export * from "./Badge";
+export * from "./Spinner";
--- src/widgets/products-list/ui/InfiniteProductsList.tsx
+++ src/widgets/products-list/ui/InfiniteProductsList.tsx
@@ -4,13 +4,16 @@ import { twMerge } from "tailwind-merge";
 import { ProductsList } from "./ProductsList";
 import { useInfiniteProductsQuery } from "../../../entities/product/model";
 import { API_GET_LIMIT } from "../../../shared/config";
+import { Spinner } from "../../../shared/ui";
 
 interface InfiniteProductsListProps {
   className?: string;
 }
 
 export function InfiniteProductsList({ className }: InfiniteProductsListProps) {
-  const { data, isLoading, isError, fetchNextPage, hasNextPage } = useInfiniteProductsQuery({ limit: API_GET_LIMIT });
+  const { data, isLoading, isError, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteProductsQuery({
+    limit: API_GET_LIMIT,
+  });
   const { ref, inView } = useInView({ threshold: 0, rootMargin: "200px 0px" });
 
   const products = data?.pages.flatMap(p => p.products) || [];
@@ -19,13 +22,14 @@ export function InfiniteProductsList({ className }: InfiniteProductsListProps) {
     if (inView && hasNextPage && !isLoading) fetchNextPage();
   }, [inView, hasNextPage, isLoading, fetchNextPage]);
 
-  if (isLoading) return <div>Loading products...</div>;
+  if (isLoading) return <Spinner />;
   if (isError) return <div>Failed to load products.</div>;
 
   return (
     <section className={twMerge("py-4", className)}>
       <ProductsList products={products} />
       <div ref={ref} className="pt-8 pb-4 text-center">
+        {isFetchingNextPage && <Spinner />}
         {!hasNextPage && <span>더 이상 상품이 존재하지 않습니다</span>}
       </div>
     </section>
