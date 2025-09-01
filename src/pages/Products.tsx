5924fa9481435c285ae10f40acc2d9a4f6bd1ba5
jong-k <fully.charged07@gmail.com>
2025-09-01 12:43:00 +0900

    feat: 데이터 페칭을 페이지에서 widget으로 변경

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,11 +1,5 @@
-import { useProductsQuery } from "../entities/product/model";
 import { ProductsList } from "../widgets/products-list/ui";
 
 export default function Products() {
-  const { data, isLoading, isError } = useProductsQuery({ limit: 20 });
-
-  if (isLoading) return <div>Loading products...</div>;
-  if (isError) return <div>Failed to load products.</div>;
-
-  return <div>{data && <ProductsList products={data.products} />}</div>;
+  return <ProductsList />;
 }
--- src/widgets/products-list/ui/ProductsList.tsx
+++ src/widgets/products-list/ui/ProductsList.tsx
@@ -1,21 +1,26 @@
 import { twMerge } from "tailwind-merge";
-import type { Product } from "../../../entities/product/model";
+import { useProductsQuery } from "../../../entities/product/model";
 import { ProductCard } from "../../../entities/product/ui";
 
 interface ProductsListProps {
-  products: Product[];
   className?: string;
 }
 
-export function ProductsList({ products, className }: ProductsListProps) {
+export function ProductsList({ className }: ProductsListProps) {
+  const { data, isLoading, isError } = useProductsQuery({ limit: 20 });
+
+  if (isLoading) return <div>Loading products...</div>;
+  if (isError) return <div>Failed to load products.</div>;
+
   return (
     <section className={twMerge("py-4", className)}>
       <ul className="grid grid-cols-1 gap-4 md:grid-cols-2">
-        {products.map(p => (
-          <li key={p.id}>
-            <ProductCard product={p} />
-          </li>
-        ))}
+        {data &&
+          data.products.map(p => (
+            <li key={p.id}>
+              <ProductCard product={p} />
+            </li>
+          ))}
       </ul>
     </section>
   );
