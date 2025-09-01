88b1c5d4c04d012f3382fa42794f0a176fbee156
jong-k <fully.charged07@gmail.com>
2025-09-01 14:12:33 +0900

    feat: 무한스크롤 적용

--- src/widgets/products-list/ui/InfiniteProductsList.tsx
+++ src/widgets/products-list/ui/InfiniteProductsList.tsx
@@ -2,27 +2,29 @@ import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 import { twMerge } from "tailwind-merge";
 import { ProductsList } from "./ProductsList";
-import { useProductsQuery } from "../../../entities/product/model";
+import { useInfiniteProductsQuery } from "../../../entities/product/model";
+import { API_GET_LIMIT } from "../../../shared/config";
 
 interface InfiniteProductsListProps {
-  limit: number;
   className?: string;
 }
 
-export function InfiniteProductsList({ limit, className }: InfiniteProductsListProps) {
-  const { data, isLoading, isError } = useProductsQuery({ limit });
+export function InfiniteProductsList({ className }: InfiniteProductsListProps) {
+  const { data, isLoading, isError, fetchNextPage, hasNextPage } = useInfiniteProductsQuery({ limit: API_GET_LIMIT });
   const { ref, inView } = useInView({ threshold: 0, rootMargin: "200px 0px" });
 
+  const products = data?.pages.flatMap(p => p.products) || [];
+
   useEffect(() => {
-    if (inView) console.log(inView);
-  }, [inView]);
+    if (inView && hasNextPage && !isLoading) fetchNextPage();
+  }, [inView, hasNextPage, isLoading, fetchNextPage]);
 
   if (isLoading) return <div>Loading products...</div>;
   if (isError) return <div>Failed to load products.</div>;
 
   return (
     <section className={twMerge("py-4", className)}>
-      {data && <ProductsList products={data?.products} />}
+      <ProductsList products={products} />
       <div ref={ref} className=""></div>
     </section>
   );
